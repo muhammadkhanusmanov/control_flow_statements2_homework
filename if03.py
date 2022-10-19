0879b3d3e73c5b740954446c1160d2afe2ba247c
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-10-19 22:53:14 +0500

    3-masala

--- if03.py
+++ if03.py
@@ -8,11 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    if (a>b and b>c) or (c>b and b>a):
+    if (a>=b and b>=c) or (c>=b and b>=a):
         d = b
-    if (b>a and a>c) or (c>a and a>b):
+    if (b>=a and a>=c) or (c>=a and a>=b):
         d = a 
-    if (a>c and c>b) or (b>c and c>a):
+    if (a>=c and c>=b) or (b>=c and c>=a):
         d = c
     return d
 
