0ea04178e42ea29af816b9b96670b3f3fdbcf1b3
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-10-19 22:13:20 +0500

    5-masala

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a1 = n%10
+    a2 = (n%100)//10
+    a3 = (n%1000)//100
+    a4 = (n%10000)//1000
+    a5 = n//10000
+    m = a1
+    if a2>m:
+        m = a2
+    if a3>m:
+        m = a3
+    if a4>m:
+        m = a4
+    if a5>m:
+        m = a5
+
+    return m 
